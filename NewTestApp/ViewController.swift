b5b27fdf8efae77286358ceb499d9ebdcf70bad4
kuri1234 <kurihara12345takeshi67890@gmail.com>
2018-08-18 20:48:01 +0900

    Initial commit.

--- NewTestApp/ViewController.swift
+++ NewTestApp/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // input method limited with "number pad"
+        self.TextNumber.keyboardType = UIKeyboardType.numberPad
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var TextNumber: UITextField!
+    
+    @IBAction func plus(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! + 1)
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! - 1)
+    }
 
+    @IBAction func plus10(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! + 10)
+    }
+    
+    @IBAction func minus10(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! - 10)
+    }
+    
+    @IBAction func plus100(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! + 100)
+    }
+    
+    @IBAction func minus100(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! - 100)
+   }
+    
+    @IBAction func plus1000(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! + 1000)
+    }
+    
+    @IBAction func minus1000(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! - 1000)
+    }
+    
+    @IBAction func plus10000(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! + 10000)
+    }
+    
+    @IBAction func minus10000(_ sender: Any) {
+        TextNumber.text = String( Int(TextNumber.text!)! - 10000)
+    }
+    
 }
 
